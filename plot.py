247b3893217feaf3e0f60bfddcd6116bff0b95c3
henry300 <henry.teigar@gmail.com>
2015-11-19 04:14:34 +0200

    Add basic idea structure to plot.py. No implementation

---
+++ plot.py
@@ -0,0 +1,18 @@
+plot = {
+    0: {
+        ('blue_heli', 'sin_passing')
+    },
+    1: {
+        ('blue_heli', 'same_position'),
+        ('blue_heli', 'same_position')
+    },
+    2: {
+        ('fighter_heli', 'circling'),
+        ('blue_heli', 'cos_passing')
+    }
+    # ...
+}
+
+
+
+
